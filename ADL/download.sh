74eb1c3993ea3bb412d75225f1a0bf41d94ba524
changcc0716057 <55429233+changcc0716057@users.noreply.github.com>
2022-11-09 20:07:57 +0800

    Delete ADL directory

--- ADL/download.sh
+++
@@ -1,3 +0,0 @@
-# Download caches and model
-wget https://www.dropbox.com/s/6ydyk2iagb02m7i/hw1.zip?dl=1 -O hw1.zip
-unzip hw1.zip
--- ADL/slot_tag.sh
+++
@@ -1,3 +0,0 @@
-# "${1}" is the first argument passed to the script
-# "${2}" is the second argument passed to the script
-python test_slot.py --test_file "${1}" --ckpt_path ./ckpt/slot/best_slot.pt --pred_file "${2}"
